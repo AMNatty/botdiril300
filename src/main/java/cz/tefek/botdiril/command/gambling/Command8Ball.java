c59690a1da4e13d01ab4cfc958e768bd096a5d4a
Michal <493msi@gmail.com>
2018-09-25 22:59:53 +0200

    Added 8ball command, how original.

---
+++ src/main/java/cz/tefek/botdiril/command/gambling/Command8Ball.java
@@ -0,0 +1,36 @@
+package cz.tefek.botdiril.command.gambling;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+
+import cz.tefek.botdiril.Botdiril;
+import cz.tefek.botdiril.framework.command.CallObj;
+import cz.tefek.botdiril.framework.command.Command;
+import cz.tefek.botdiril.framework.command.CommandCategory;
+import cz.tefek.botdiril.framework.command.invoke.CmdInvoke;
+import cz.tefek.botdiril.framework.command.invoke.CmdPar;
+import cz.tefek.botdiril.framework.util.CommandAssert;
+import cz.tefek.botdiril.framework.util.MR;
+
+@Command(value = "8ball", category = CommandCategory.GAMBLING, aliases = {}, description = "Ask the 8ball.")
+public class Command8Ball
+{
+    private static final String[] answers = { "It is certain.", "It is decidedly so.", "Without a doubt.",
+            "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.",
+            "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.",
+            "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
+            "My sources say no.", "Outlook not so good.", "Very doubtful." };
+
+    @CmdInvoke
+    public static void roll(CallObj co, @CmdPar("question") String question)
+    {
+        CommandAssert.stringNotTooLong(question, 300, "Your question is too long, please make it shorter.");
+
+        var eb = new EmbedBuilder();
+        eb.setAuthor(co.callerMember.getEffectiveName() + "'s question");
+        eb.setDescription(question);
+        eb.setColor(0x008080);
+        eb.addField("Answer:", answers[Botdiril.RDG.nextInt(0, answers.length)], false);
+
+        MR.send(co.textChannel, eb.build());
+    }
+}
